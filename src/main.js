094ada90870297b616363014ccd0dc1a893f3c63
joostlek <joostlek@outlook.com>
2019-10-23 15:02:26 +0200

    Remove console.log

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ import { Auth0Plugin } from './auth';
 
 Vue.config.productionTip = false;
 
-console.log(process.env.TRAVIS_BRANCH);
-
 if (process.env.VUE_APP_BRANCH === 'master' && process.env.NODE_ENV === 'production') {
   Vue.use(Auth0Plugin, {
     domain,
