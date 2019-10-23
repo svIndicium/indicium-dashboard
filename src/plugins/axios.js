c82588ecb93e6030ef49c75fab382b495199e5a1
joostlek <joostlek@outlook.com>
2019-10-23 13:34:28 +0200

    Add link to dev version of lit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -6,7 +6,7 @@ const instance = axios.create();
 Vue.prototype.$http = instance;
 
 const apiInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://lit.indicium.hu' : 'http://localhost:8080',
+  baseURL: process.env.TRAVIS_BRANCH === 'dev' ? 'https://lit.dev.indicium.hu' : process.env.NODE_ENV === 'production' ? 'https://lit.indicium.hu' : 'http://localhost:8080',
 });
 
 apiInstance.defaults.headers.common.Authorization = `Bearer ${JSON.parse(localStorage.getItem('token'))}`;
