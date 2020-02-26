2becc2f27b4f95c115f204c8e025491eb3737c90
joostlek <joostlek@outlook.com>
2020-02-26 15:21:34 +0100

    Fix axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -8,11 +8,11 @@ Vue.prototype.$http = instance;
 
 const baseURL = process.env.VUE_APP_BRANCH === 'dev'
     ? 'https://lit.dev.indicium.hu'
-    : process.env.NODE_ENV === 'production'
+    : process.env.NODE_ENV === 'production' && process.env.VUE_APP_BRANCH === 'master'
         ? 'https://lit.indicium.hu'
         : 'https://lit.dev.indicium.hu'
 
-const apiInstance = axios.create({ baseURL: 'https://lit.dev.indicium.hu' });
+const apiInstance = axios.create({ baseURL: baseURL });
 
 apiInstance.defaults.headers.common.Authorization = `Bearer ${JSON.parse(localStorage.getItem('token'))}`;
 
