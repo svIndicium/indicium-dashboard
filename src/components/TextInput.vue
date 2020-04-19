71a5df17490c89e1a9e1cf0156a4cd8421508252
joostlek <joostlek@outlook.com>
2020-04-20 01:01:04 +0200

    Redesign inputfield

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,31 +1,46 @@
 <template>
-  <div>
-    <input type="text" v-model="internalValue" @input="update" :placeholder="placeholder">
-  </div>
+    <div>
+        <input type="text" v-model="internalValue" @input="update" :placeholder="placeholder" :class="[!!error ? 'error' : '']">
+        <div class="error-message">{{error}}</div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'TextInput',
-  props: ['value', 'placeholder', 'label'],
-  data() {
-    return {
-      internalValue: this.value,
+    export default {
+        name: 'TextInput',
+        props: ['value', 'placeholder', 'label', 'error'],
+        data() {
+            return {
+                internalValue: this.value,
+            };
+        },
+        methods: {
+            update() {
+                this.$emit('input', this.internalValue);
+            },
+        },
     };
-  },
-  methods: {
-    update() {
-      this.$emit('input', this.internalValue);
-    },
-  },
-};
 </script>
 
-<style scoped>
-input {
-  border: 1px #CCCCCC solid;
-  padding: 4px;
-  border-radius: 5px;
-  margin: 4px;
-}
+<style lang="scss" scoped>
+    input {
+        border: 2px var(--indi-grey) solid;
+        padding: 8px 16px;
+        border-radius: 8px;
+
+        &:focus {
+            border: 2px var(--indi-blue) solid;
+            outline: none;
+        }
+
+        &.error {
+            border: 2px #E07272 solid;
+        }
+    }
+
+    .error-message {
+        margin-left: 16px;
+        color: #E07272;
+        font-size: 12px;
+    }
 </style>
