d633080e29eaead4d0d68de30b2bb066f8fe1850
joostlek <joostlek@outlook.com>
2020-04-20 16:23:44 +0200

    Enhance profile loading

--- src/views/profile/ViewProfile.vue
+++ src/views/profile/ViewProfile.vue
@@ -1,47 +1,64 @@
 <template>
     <div>
         <h2>Profiel</h2>
-        <div class="section">
-            <h3 class="section-header">Algemene informatie</h3>
-            <div class="section-entry">
-                <p class="key">Voornaam</p>
-                <p class="value">{{this.user.given_name}}</p>
-            </div>
-            <div class="section-entry">
-                <p class="key">Achternaam</p>
-                <p class="value">{{this.user.family_name}}</p>
-            </div>
-            <div class="section-entry">
-                <p class="key">Geboortedatum</p>
-                <p class="value">{{this.getPrettyDate(this.user.dateOfBirth)}}</p>
-            </div>
+        <div v-if="loading">
+            <Loading />
         </div>
-        <div class="section">
-            <h3 class="section-header">Contact informatie</h3>
-            <div class="section-entry">
-                <p class="key">Telefoonnummer</p>
-                <p class="value">{{prettyPhoneNumber}}</p>
+        <div v-else>
+            <div class="section">
+                <h3 class="section-header">Algemene informatie</h3>
+                <div class="section-entry">
+                    <p class="key">Voornaam</p>
+                    <p class="value">{{this.user.firstName}}</p>
+                </div>
+                <div class="section-entry">
+                    <p class="key">Achternaam</p>
+                    <p class="value">{{this.user.lastName}}</p>
+                </div>
+                <div class="section-entry">
+                    <p class="key">Geboortedatum</p>
+                    <p class="value">{{this.getPrettyDate(this.user.dateOfBirth)}}</p>
+                </div>
             </div>
-            <div class="section-entry">
-                <p class="key">Primaire e-mailadres</p>
-                <p class="value">{{this.user.email}}</p>
+            <div class="section">
+                <h3 class="section-header">Opleidingsinformatie</h3>
+                <div class="section-entry" v-if="this.user.studyType">
+                    <p class="key">Studierichting</p>
+                    <p class="value">{{this.user.studyType.name}}</p>
+                </div>
             </div>
-        </div>
-        <div class="section">
-            <h3 class="section-header">Profiel informatie</h3>
-            <div class="section-entry">
-                <p class="key">Laatst bewerkt op</p>
-                <p class="value">{{this.getPrettyDateTime(this.user.updated_at)}}</p>
+            <div class="section">
+                <h3 class="section-header">Contact informatie</h3>
+                <div class="section-entry">
+                    <p class="key">Telefoonnummer</p>
+                    <p class="value">{{prettyPhoneNumber}}</p>
+                </div>
+                <div class="section-entry">
+                    <p class="key">Primaire e-mailadres</p>
+                    <p class="value">{{this.user.email}}</p>
+                </div>
+            </div>
+            <div class="section">
+                <h3 class="section-header">Profiel informatie</h3>
+                <div class="section-entry">
+                    <p class="key">Laatst bewerkt op</p>
+                    <p class="value">{{this.getPrettyDateTime(this.user.updated_at)}}</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Loading from '../../components/Loading';
+
     export default {
         name: 'ViewProfile',
+        components: {
+            Loading,
+        },
         data: () => ({
-            user: null,
+            user: {},
             loading: false,
         }),
         methods: {
@@ -51,6 +68,11 @@
             async getUserFromService() {
                 const { data } = await this.$api.get('/user/3');
                 this.user = {...this.user, ...data};
+                await this.getStudyType();
+            },
+            async getStudyType() {
+                const { data } = await this.$api.get(`/studytype/${this.user.studyTypeId}`);
+                this.user.studyType = data;
             },
             getMonthAsString(currentMonth) {
                 const monthList = [
@@ -80,8 +102,10 @@
             },
         },
         async mounted() {
+            this.loading = true;
             this.getUser();
             await this.getUserFromService();
+            this.loading = false;
         },
         computed: {
             prettyPhoneNumber() {
