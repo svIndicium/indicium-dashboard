fb3ca25f0b102a5cdcd82b068af9096b5f3d94ec
joostlek <joostlek@outlook.com>
2021-03-05 11:51:38 +0100

    Create mail address service

--- src/services/index.js
+++ src/services/index.js
@@ -2,9 +2,11 @@ import MemberService from './MemberService';
 import StudyTypeService from "./StudyTypeService";
 import RegistrationService from "./RegistrationService";
 import EventService from "./EventService";
+import MailAddressService from "./MailAddressService";
 
 export {
     MemberService,
+    MailAddressService,
     StudyTypeService,
     RegistrationService,
     EventService,
