7262feea93e52cb273e12d1f0f16d9a187982141
joostlek <joostlek@outlook.com>
2021-04-23 16:58:40 +0200

    Update event view page

--- src/views/events/ViewEvent.vue
+++ src/views/events/ViewEvent.vue
@@ -9,34 +9,52 @@
         </v-row>
         <v-row>
             <v-col>
-                <v-card v-if="event">
+                <v-card>
                     <v-card-title>{{ event.title }}</v-card-title>
-                    <v-tabs>
-                        <v-tab
-                            :to="{name: 'ProfileViewInfo'}"
-                        >
-                            <v-icon>mdi-information</v-icon>
-                        </v-tab>
-                        <v-tab
-                            :to="{name: 'ProfileViewMailAddresses'}"
-                        >
-                            <v-icon>mdi-at</v-icon>
-                        </v-tab>
-                        <v-tab
-                            :to="{name: 'ProfileViewMemberships'}"
-                        >
-                            <v-icon>mdi-wallet-membership</v-icon>
-                        </v-tab>
-                        <v-tab
-                            :to="{name: 'ProfileViewPayments'}"
-                        >
-                            <v-icon>mdi-currency-eur</v-icon>
-                        </v-tab>
-                    </v-tabs>
                 </v-card>
             </v-col>
         </v-row>
-        <router-view></router-view>
+        <v-row>
+            <v-col
+            cols="4">
+                <v-card>
+                    <v-img :src="event.image" ></v-img>
+                    <v-card-text v-text="event.description"></v-card-text>
+                </v-card>
+            </v-col>
+            <v-col
+            cols="2">
+                <v-row>
+                    <v-col>
+                        <v-card>
+                            <v-card-text>
+                                <v-text-field :value="this.$utils.getPrettyDateTime(event.start)" label="Begint op" disabled></v-text-field>
+                                <v-text-field :value="this.$utils.getPrettyDateTime(event.end)" label="Eindigt op" disabled></v-text-field>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-card>
+                            <v-card-text>
+                                <template v-if="event.url === ''">Je hoeft je voor deze activiteit niet in te schrijven!</template>
+                                <v-btn :disabled="event.url === ''" color="primary" :href="event.url" target="_blank" plain>Inschrijven</v-btn>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <v-card>
+                            <v-card-text>
+                                <v-btn @click="shareEvent" color="primary" plain>Deel deze activiteit</v-btn>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
